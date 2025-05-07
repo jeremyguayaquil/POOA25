d7fe3ac7999a656724e8cf3ba8bcb0df189fcfa2
Walther Gaibor <wgaibor@teclemas.edu.ec>
2025-05-06 21:31:11 -0500

    HolaMundo

---
+++ HolaMundo/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    // scope
+    public static void main(String[] args) throws Exception {
+        // Variable
+        int numero1 = 10;
+        int a = 10;
+        System.out.println("Hola Mundo!");
+    }
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}

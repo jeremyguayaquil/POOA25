ea01cd91bb570974a89dd8e55a4a320b7aa0cfb7
Walther Gaibor <wgaibor@teclemas.edu.ec>
2025-05-26 20:18:58 -0500

    Uso del For

---
+++ Buclefor/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * System.out.println("1");
+         * System.out.println("2");
+         * System.out.println("3");
+         * System.out.println("4");
+         * System.out.println("5");
+         * System.out.println("6");
+         * System.out.println("7");
+         * System.out.println("8");
+         * System.out.println("9");
+         * System.out.println("10");
+         * System.out.println("11");
+         * System.out.println("12");
+         * System.out.println("13");
+         * System.out.println("14");
+         */
+
+        // =============
+        // =============
+        for (int incremento = 0; incremento <= 100; incremento++) {
+            System.out.println("Número  " + incremento);
+        }
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        // Mostrar por consola número en forma descendente del 100 al 1
+        for (int decremento = 100; decremento > 1; decremento--) {
+            System.out.println("Número decrementado  " + decremento);
+        }
+
+    }
+}

f555289672ec0a284f67a3f0671021f83856a7e2
Walther Gaibor <wgaibor@teclemas.edu.ec>
2025-05-26 20:29:57 -0500

    uso del while

---
+++ BucleWhile/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int incrementador = 0;
+        while (incrementador <= 100) {
+            incrementador++;
+            System.out.println("Incemento número  " + incrementador);
+        }
+    }
+}
